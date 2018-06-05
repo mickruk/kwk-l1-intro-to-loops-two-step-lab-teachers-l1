16eec8249cfcca8cdbfb0adf5d95722030c47418
Michelle Kruk <kruk.mich@gmail.com>
2018-06-05 21:32:51 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,12 @@
+def dance 
+  moves_completed = 0 
+  
+  while moves_completed < 10 
+  puts "to the left to the left"
+  puts "walk it out"
+  
+  moves_completed += 1 
+  #moves_completed = moves_completed + 1
+  
+  puts "#{moves_completed}"
+end
